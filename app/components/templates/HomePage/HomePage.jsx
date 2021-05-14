ed4e5c40ba2a513b82bc841f8f16d2cdad32ddc2
dbaonam99 <dbaonam99@gmail.com>
2021-05-14 18:58:21 +0700

    Create Button Element

--- app/components/templates/HomePage/HomePage.jsx
+++ app/components/templates/HomePage/HomePage.jsx
@@ -1,5 +1,25 @@
+import Button from "@element/Button/Button";
+
 function HomePage() {
-  return <div>This is home page</div>
+  return <div>
+    <h3>Elements</h3>
+    <div style={{
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'center'
+    }}>
+      <Button
+        label="PLAY VIDEO"
+        margin={'10px 10px 10px 10px'}
+      />
+      <br/>
+      <Button
+        label="BUY VIDEO"
+        redBg
+        margin={'10px 10px 10px 10px'}
+      />
+    </div>
+  </div>
 }
 
 export default HomePage
