fc5551fdaa1bc443f39c2bf214d9272a005ddbc9
dbaonam99 <dbaonam99@gmail.com>
2021-05-17 22:42:18 +0700

    Shop responsive is done

--- app/components/layouts/Default/Default.jsx
+++ app/components/layouts/Default/Default.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import LanguageSelection from 'components/elements/LanguageSelection/LanguageSelection';
 import Header from 'components/modules/Header/Header';
 import Icon from 'components/elements/Icon/Icon';
@@ -7,12 +8,16 @@ import Facebook from '../../../../public/icons/fb.svg';
 import Instagram from '../../../../public/icons/ig.svg';
 import Youtube from '../../../../public/icons/youtube.svg';
 import Spotify from '../../../../public/icons/spotify.svg';
-import AppleMusic from '../../../../public/icons/fb-dark.svg';
+import AppleMusic from '../../../../public/icons/apple-music.svg';
 import styles from './Default.module.scss';
 
 function Default({ children }) {
   return (
     <div className={styles.default}>
+      <Head>
+        <title>SUBOI</title>
+        <meta charSet="utf-8" /><meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width, height=device-height" />
+      </Head>
       <Header />
       <LanguageSelection />
       <div className={styles.content}>{children}</div>
