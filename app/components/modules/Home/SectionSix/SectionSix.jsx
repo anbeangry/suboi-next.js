bde2feacaa67491c8a2a14db9d1c7a92a015d935
M Difa Ananda <difacool40@gmail.com>
2021-05-20 04:59:58 +0800

    Add section six

---
+++ app/components/modules/Home/SectionSix/SectionSix.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from 'elements/Button/Button';
+import styles from './SectionSix.module.scss';
+
+function SectionTwo() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.info}>
+          <div>
+            <h2>Business Inquiries</h2>
+            <p>suboibooking@gmail.com</p>
+          </div>
+          <div>
+            <h2>Management</h2>
+            <p>kiwi@mau.network</p>
+          </div>
+        </div>
+        <div className={styles.newsletter}>
+          <h2>Newsletter</h2>
+          <input type="text" placeholder="Enter Your Email" />
+          <Button
+            background="transparent"
+            label="SUBSCRIBE"
+            onClick={() => alert('You subscribed')}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SectionTwo;
--- app/components/templates/Home/Home.jsx
+++ app/components/templates/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SectionOne from 'modules/Home/SectionOne/SectionOne';
 import SectionTwo from 'modules/Home/SectionTwo/SectionTwo';
+import SectionSix from 'modules/Home/SectionSix/SectionSix';
 
 function HomePage() {
   return (
     <React.Fragment>
       <SectionOne />
       <SectionTwo />
+      <SectionSix />
     </React.Fragment>
   );
 }
