aa212b217c3eecd49ce7eff5b34dc86db0490fbf
M Difa Ananda <difacool40@gmail.com>
2021-05-24 15:32:38 +0800

    Adding some missing components

--- app/components/templates/Home/Home.jsx
+++ app/components/templates/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SectionOne from 'modules/Home/SectionOne/SectionOne';
 import SectionTwo from 'modules/Home/SectionTwo/SectionTwo';
+import SectionThree from 'modules/Home/SectionThree/SectionThree';
 import SectionFour from 'modules/Home/SectionFour/SectionFour';
+import SectionFive from 'modules/Home/SectionFive/SectionFive';
 import SectionSix from 'modules/Home/SectionSix/SectionSix';
 
-function HomePage() {
+function HomePage({ songs, photos }) {
   return (
     <React.Fragment>
       <SectionOne />
       <SectionTwo />
+      <SectionThree songs={songs}/>
       <SectionFour />
+      <SectionFive photos={photos}/>
       <SectionSix />
     </React.Fragment>
   );
