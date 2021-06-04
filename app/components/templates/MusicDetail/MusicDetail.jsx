7c9e34a35034b96509219d93476b0ada50202fbb
dbaonam99 <dbaonam99@gmail.com>
2021-06-04 09:42:27 +0700

    Fix music detail background

--- app/components/templates/MusicDetail/MusicDetail.jsx
+++ app/components/templates/MusicDetail/MusicDetail.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SongDetail from 'modules/Music/SongDetail/SongDetail';
 import Background from 'elements/Background/Background';
+import { urlFor } from 'utils/sanity';
 import styles from './MusicDetail.module.scss';
 
 function MusicDetail({ item }) {
   return <div className={styles.container}>
     <Background
-      url="https://avatar-ex-swe.nixcdn.com/song/2019/12/31/2/b/5/a/1577798006327_640.jpg"
+      url={urlFor(item.coverImage.asset._ref)}
       className={styles.background}
     />
     <SongDetail item={item}/>
