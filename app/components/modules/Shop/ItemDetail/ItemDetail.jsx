7af24cda4e3f1032989fbcc2a3b1a09b9065a84b
dbaonam99 <dbaonam99@gmail.com>
2021-06-05 17:16:17 +0700

    Change Image preview style

--- app/components/modules/Shop/ItemDetail/ItemDetail.jsx
+++ app/components/modules/Shop/ItemDetail/ItemDetail.jsx
@@ -9,17 +9,13 @@ import styles from './ItemDetail.module.scss';
 function Item({ item, showPopup, openCart }) {
   const [selectedSize, setSelectedSize] = useState('');
   const [selectedPrice, setSelectedPrice] = useState('');
-  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState('');
   const [error, setError] = useState('');
   const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
     if (!openCart) setSelectedSize('');
   }, [openCart]);
-
-  useEffect(() => {
-    setImages(item.productImage);
-  }, []);
 
   const handleOnClick = () => {
     if (item.productVariant.length > 0) {
@@ -42,26 +38,18 @@ function Item({ item, showPopup, openCart }) {
     setSelectedPrice(size.price);
   };
 
-  const chooseImage = (image, index) => {
-    const rearrangeImage = [...images];
-    rearrangeImage.splice(index, 1);
-    rearrangeImage.unshift(image);
-    setImages(rearrangeImage);
-  };
-
   return <div className={styles.itemDetail}>
     <div className={styles.left}>
-      {images.length > 0 && <img
-          className={styles.itemImage}
-          src={urlFor(images[0]?.asset._ref)}
-        />
-      }
+      <img
+        className={styles.itemImage}
+        src={urlFor(selectedImage || item.productImage[0]?.asset._ref)}
+      />
       <div className={styles.smallImages}>
-        {images.length > 1 && images.slice(1).map((url, index = 1) => <img
+        {item.productImage?.map((url, index = 1) => <img
           key={index}
           className={styles.smallImageItem}
           src={urlFor(url?.asset._ref)}
-          onClick={() => chooseImage(url, index + 1)}
+          onClick={() => setSelectedImage(url)}
         />)}
       </div>
     </div>
