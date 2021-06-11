a85d3497e8917207179819ed6cd7509d38b487ab
dbaonam99 <dbaonam99@gmail.com>
2021-06-11 18:41:49 +0700

    Add preload back

--- app/components/elements/Background/Background.jsx
+++ app/components/elements/Background/Background.jsx
@@ -7,7 +7,7 @@ import styles from './Background.module.scss';
 function Background({ url, className }) {
   return (
     <div className={clsx(styles.background, className)}>
-      <Image src={url} layout='fill'/>
+      <Image src={url} layout='fill' priority={true}/>
     </div>
   );
 }
