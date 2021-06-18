3bd66f5c4017705c392f44e46c6f82d47eabc8b0
dbaonam99 <dbaonam99@gmail.com>
2021-06-18 09:38:57 +0700

    Add Music embedded

--- app/components/modules/Home/SectionFive/SectionFive.jsx
+++ app/components/modules/Home/SectionFive/SectionFive.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
-// import ReactPlayer from 'react-player/youtube';
+import ReactPlayer from 'react-player/youtube';
+import { isMobile } from 'react-device-detect';
 import Button from 'elements/Button/Button';
 import ArrowRight from 'public/icons/arrow-right.svg';
 import { urlFor } from 'utils/sanity';
@@ -12,15 +13,15 @@ import styles from './SectionFive.module.scss';
 
 function SectionFive({ height, videos }) {
   const router = useRouter();
-  const playerRef = useRef();
   const [isOpenVideo, setIsOpenVideo] = useState(false);
+  const [currentVideo, setCurrentVideo] = useState(0);
 
-  const handleOnClick = () => {
-    if (isOpenVideo) {
-      playerRef.current.focus();
-      setIsOpenVideo(false);
+  const handleOnClick = (url, index) => {
+    if (isMobile) {
+      router.push(url);
     } else {
       setIsOpenVideo(true);
+      setCurrentVideo(index);
     }
   };
 
@@ -29,37 +30,36 @@ function SectionFive({ height, videos }) {
       className={styles.container}
       style={{ height: `${height}px` }}
     >
-      <div className={clsx(styles.iframe, isOpenVideo && styles.iframeOpen)}>
-        {/* <ReactPlayer
-          controls
-          playing={isOpenVideo}
-          width='50%'
-          height='50%'
-          url="https://www.youtube.com/embed/CZJvBfoHDk0"
-          ref={playerRef}
-        /> */}
-      </div>
       <div className={styles.videos}>
         {videos?.map((item, index) => <div key={index} className={styles.videoItem}>
-            <div className={styles.overlay}></div>
-            <div className={styles.videoImg}>
-              <Image
-                src={urlFor(item.thumbnail).url()}
-                className={styles.nextImg}
-                layout='fill'
-                priority={true}
+          {currentVideo === index + 1 && <div className={styles.iframe}>
+              <ReactPlayer
+                controls
+                playing={isOpenVideo}
+                width='100%'
+                height='100%'
+                url={item.youtubeEmbedLink}
               />
             </div>
-            <div className={styles.action}>
-              <div className={styles.smallText}>{item.title}</div>
-              <Button
-                label="PLAY VIDEO"
-                className={styles.button}
-                onClick={handleOnClick}
-              />
-            </div>
-          </div>)
-        }
+          }
+          <div className={styles.overlay}></div>
+          <div className={styles.videoImg}>
+            <Image
+              src={urlFor(item.thumbnail).url()}
+              className={styles.nextImg}
+              layout='fill'
+              priority={true}
+            />
+          </div>
+          <div className={styles.action}>
+            <div className={styles.smallText}>{item.title}</div>
+            <Button
+              label="PLAY VIDEO"
+              className={styles.button}
+              onClick={() => handleOnClick(item.youtubeEmbedLink, index + 1)}
+            />
+          </div>
+          </div>)}
         <div className={styles.viewAll} onClick={() => router.push('/video')}>
           <div className={styles.icon}>
             <ArrowRight className={styles.white}/>
