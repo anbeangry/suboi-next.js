0baafa89b3ebb997793729e40c7b3af27a2034a1
M Difa Ananda <difacool40@gmail.com>
2021-06-23 23:14:31 +0800

    Add some meta tags

--- pages/video.jsx
+++ pages/video.jsx
@@ -4,6 +4,8 @@ import { groq } from 'next-sanity';
 import { getClient } from 'utils/sanity';
 import Default from 'layouts/Default/Default';
 import VideoTemplate from 'templates/Video/Video';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 const query = groq`
   {
@@ -39,7 +41,20 @@ export async function getStaticProps({ preview = false }) {
 }
 
 function Video({ data }) {
+  const { locale } = useRouter();
+  const title = locale === 'en_US'
+    ? 'Checkout lastest video from Suboi'
+    : 'Xem các video mới nhất từ Suboi';
+
   return <Default social={data.contact.social}>
+    <Head>
+      {/* Open Graph */}
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="SUBOI" />
+    </Head>
     <VideoTemplate video={data.video} />
   </Default>;
 }
