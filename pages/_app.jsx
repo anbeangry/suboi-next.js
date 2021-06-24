92f7e8907b40eca4f74b96cf544c974da72a0a10
dbaonam99 <dbaonam99@gmail.com>
2021-06-24 16:22:31 +0700

    Implement GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import * as gtag from 'utils/gtag';
 import { CartProvider } from '../app/contexts/Cart';
 import { CategoryProvider } from '../app/contexts/Category';
 import '../app/styles/base/_base.scss';
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <CategoryProvider>
     <CartProvider>
       <Component {...pageProps} />
